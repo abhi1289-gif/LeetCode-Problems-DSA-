14f3eb68d78bd259c84c10fe18a255ae3a27cd70
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-08 12:44:40 +0530

    Time: 9 ms (55.19%) | Memory: 46.7 MB (28.10%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,33 @@
 class Solution {
 
-    int ans = 0;
+    int dp[][];
+    int ofset;
 
-    public void make(int idx, int sum, int target, int nums[]){
+    public int make(int idx, int sum, int target, int nums[]){
         if(idx == nums.length){
-            if(target == sum){
-                ans++;
-            }
-            return;
+            return (sum == target)? 1: 0;
         }
+        if(dp[idx][ofset+sum] != -1) return dp[idx][ofset+sum];
 
-        make(idx+1, sum-nums[idx], target, nums);
-        make(idx+1, sum+nums[idx], target, nums);
+        int a = make(idx+1, sum-nums[idx], target, nums);
+        int b = make(idx+1, sum+nums[idx], target, nums);
+
+        return dp[idx][ofset+sum] = a+b;
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        make(0, 0, target, nums);
-        return ans;
+        int t = 0;
+        for(int i=0; i<nums.length; i++) t += nums[i];
+        ofset = t;
+        if(Math.abs(target) > t) return 0;
+
+        dp = new int[nums.length][t*2+1];
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<=t*2; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return make(0, 0, target, nums);
     }
 }
