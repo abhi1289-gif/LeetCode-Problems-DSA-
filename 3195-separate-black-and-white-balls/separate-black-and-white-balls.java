91e6121cc320146ba8e7f146106330d6051dc31b
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-14 14:57:44 +0530

    Time: 8 ms (49.84%) | Memory: 47.7 MB (36.39%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,23 +1,10 @@
 class Solution {
     public long minimumSteps(String s) {
         long ans = 0;
-
-        int n = s.length();
-        int arr[] = new int[n];
-
-        int temp = 0;
-
-        for(int i=n-1; i>=0; i--){
-            if(s.charAt(i) == '0'){
-                temp++;
-            }
-            arr[i] = temp;
-        }
-
-        for(int i=0; i<n; i++){
-            if(s.charAt(i) == '1'){
-                ans += arr[i];
-            }
+        long ones = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '1') ones++;
+            else ans += ones;
         }
 
         return ans;
