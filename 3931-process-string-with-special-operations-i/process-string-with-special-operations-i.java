3bdfbc681825265cfd808c3ad240c94feefe7b0e
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-16 09:52:02 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (75.00%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '*'){
+                if(ans.length() == 0) continue;
+                ans.deleteCharAt(ans.length()-1);
+            }
+            else if(ch == '#'){
+                ans.append(ans);
+            }
+            else if(ch == '%'){
+                ans.reverse();
+            }
+            else{
+                ans.append(ch);
+            }
+        }
+
+        return ans.toString();
+    }
+}
