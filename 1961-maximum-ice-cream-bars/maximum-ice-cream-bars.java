7b81509b9d69f6d064243253ee168e30da8e7d2b
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-21 12:12:55 +0530

    Time: 7 ms (85.10%) | Memory: 83 MB (86.97%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,6 +1,17 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+        int maxx = costs[0];
+        for(int cost: costs) maxx = Math.max(maxx, cost);
+        int count[] = new int[maxx+1];
+        for(int i=0; i<costs.length; i++) count[costs[i]]++;
+
+        int a = 0;
+        for(int i=0; i<=maxx; i++){
+            while(count[i]>0){
+                costs[a++] = i;
+                count[i]--;
+            }
+        }
         int sum = 0;
         int ans = 0;
         for(int i=0; i<costs.length; i++){
