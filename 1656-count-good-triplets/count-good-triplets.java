76702641e3114793146b8e5987a67d6c83a7c744
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-29 03:10:04 +0530

    Time: 13 ms (63.25%) | Memory: 43 MB (75.91%) - LeetSync

---
+++ 1656-count-good-triplets/count-good-triplets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countGoodTriplets(int[] nums, int a, int b, int c) {
+        int ans = 0;
+
+        for(int i=0; i<nums.length-2; i++){
+            for(int j=i+1; j<nums.length-1; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    if(Math.abs(nums[i]-nums[j]) <= a && Math.abs(nums[j]-nums[k]) <= b && Math.abs(nums[i]-nums[k]) <= c) ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
