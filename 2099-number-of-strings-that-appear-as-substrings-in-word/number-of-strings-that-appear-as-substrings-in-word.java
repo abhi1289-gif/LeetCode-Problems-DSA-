97ed95b5b5991f29bade1a17c4f757f5957b2c5f
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-29 13:08:54 +0530

    Time: 1 ms (75.75%) | Memory: 43.6 MB (19.94%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,20 +1,26 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        HashMap<String, Integer> mapp = new HashMap<>();
+        // HashMap<String, Integer> mapp = new HashMap<>();
 
-        for(String p: patterns){
-            mapp.put(p, mapp.getOrDefault(p, 0)+1);
-        }
+        // for(String p: patterns){
+        //     mapp.put(p, mapp.getOrDefault(p, 0)+1);
+        // }
 
         int ans = 0;
 
-        for(int i=0; i<word.length(); i++){
-            for(int j=i; j<word.length(); j++){
-                String a = word.substring(i, j+1);
-                if(mapp.containsKey(a)){
-                    ans += mapp.get(a);
-                    mapp.remove(a);
-                }
+        // for(int i=0; i<word.length(); i++){
+        //     for(int j=i; j<word.length(); j++){
+        //         String a = word.substring(i, j+1);
+        //         if(mapp.containsKey(a)){
+        //             ans += mapp.get(a);
+        //             mapp.remove(a);
+        //         }
+        //     }
+        // }
+
+        for(int i=0; i<patterns.length; i++){
+            if(word.contains(patterns[i])){
+                ans++;
             }
         }
 
