0aeb1ba9c83222b1489db00395a14cb97ceae212
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-30 01:57:35 +0530

    Time: 1 ms (90.51%) | Memory: 47.3 MB (52.67%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -5,27 +5,48 @@ class Solution {
             if(nums[0] > nums[1]) return 2;
             return 0;
         }
-        int[] arr = nums.clone();
-        Arrays.sort(arr);
-        int a = -1;
-        int b = -1;
 
-        for(int i=0; i<arr.length; i++){
-            if(nums[i] != arr[i]){
-                a = i;
-                break;
-            }
+        // int[] arr = nums.clone();
+        // Arrays.sort(arr);
+        // int a = -1;
+        // int b = -1;
+
+        // for(int i=0; i<arr.length; i++){
+        //     if(nums[i] != arr[i]){
+        //         a = i;
+        //         break;
+        //     }
+        // }
+
+        // if(a == -1) return 0;
+
+        // for(int i=arr.length-1; i>=0; i--){
+        //     if(nums[i] != arr[i]){
+        //         b = i;
+        //         break;
+        //     }
+        // }
+
+        // return b-a+1;
+
+        int right = -1;
+        int maxx = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < maxx) right = i;
+            maxx = Math.max(maxx, nums[i]);
         }
 
-        if(a == -1) return 0;
+        if(right == -1) return 0;
 
-        for(int i=arr.length-1; i>=0; i--){
-            if(nums[i] != arr[i]){
-                b = i;
-                break;
-            }
+        int left = -1;
+        int minn = Integer.MAX_VALUE;
+
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i] > minn) left = i;
+            minn = Math.min(minn, nums[i]);
         }
 
-        return b-a+1;
+        return right-left+1;
     }
 }
