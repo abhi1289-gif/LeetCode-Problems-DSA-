8c80d4eadd5557e7b1e9fa6cf4b9f5b836a5153d
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-30 00:10:47 +0530

    Time: 12 ms (37.70%) | Memory: 46.3 MB (80.26%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -6,22 +6,14 @@ class Solution {
             mapp.put(nums[i], mapp.getOrDefault(nums[i], 0) + 1);
         }
 
-        int arr[] = new int[mapp.size()];
-        int a = 0;
-
-        for(int num: mapp.keySet()) arr[a++] = num;
-
         int ans = 0;
 
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=i+1; j<arr.length; j++){
-                if(Math.abs(arr[i]-arr[j]) == k) ans++;
-            }
-        }
-
-        if(k==0){
-            for(int key: mapp.keySet()){
+        for(int key: mapp.keySet()){
+            if(k == 0){
                 if(mapp.get(key) > 1) ans++;
+            }
+            else{
+                if(mapp.containsKey(k+key)) ans++;
             }
         }
 
