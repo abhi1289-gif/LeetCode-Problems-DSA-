65debbb0f1cd26a4af38438f562587fec0e455fb
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-06-29 13:06:41 +0530

    Time: 13 ms (3.88%) | Memory: 46.8 MB (8.19%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        HashMap<String, Integer> mapp = new HashMap<>();
+
+        for(String p: patterns){
+            mapp.put(p, mapp.getOrDefault(p, 0)+1);
+        }
+
+        int ans = 0;
+
+        for(int i=0; i<word.length(); i++){
+            for(int j=i; j<word.length(); j++){
+                String a = word.substring(i, j+1);
+                if(mapp.containsKey(a)){
+                    ans += mapp.get(a);
+                    mapp.remove(a);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
