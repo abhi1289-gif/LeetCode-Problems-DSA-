f21736d54a7358a31b2a7a9fa031709bc1eb125a
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-07-06 19:06:06 +0530

    Time: 69 ms (25.00%) | Memory: 176.5 MB (43.92%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long[] getDistances(int[] nums) {
+        HashMap<Integer, List<Integer>> mapp = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(!mapp.containsKey(nums[i])) mapp.put(nums[i], new ArrayList<>());
+            mapp.get(nums[i]).add(i);
+        }
+
+        long ans[] = new long[nums.length];
+
+        for(List<Integer> temp : mapp.values()) {
+            int size = temp.size();
+
+            long totalSum = 0;
+            
+            for(int index : temp) {
+                totalSum += index;
+            }
+
+            long leftSum = 0;
+
+            for(int j = 0; j < size; j++) {
+                long index = temp.get(j);
+
+                long left = index * j - leftSum;
+
+                long rightSum = totalSum - leftSum - index;
+                long right = rightSum - index * (size - j - 1);
+
+                ans[(int) index] = left + right;
+
+                leftSum += index;
+            }
+        }
+
+        return ans;
+    }
+}
