f8fb08fd215bec4638512d5444463e56a84e4827
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-07-14 14:38:54 +0530

    Time: 38 ms (67.64%) | Memory: 99 MB (11.51%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int sec = Integer.MIN_VALUE;
+
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i]<sec) return true;
+
+            while(st.size() > 0 && st.peek() < nums[i]){
+                sec = st.pop();
+            }
+
+            st.add(nums[i]);
+        }
+
+        return false;
+    }
+}
