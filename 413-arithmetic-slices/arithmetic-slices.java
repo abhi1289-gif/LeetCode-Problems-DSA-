d448732240959c30309d796451669ebd2ea10fcc
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-07-15 18:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (98.01%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.java
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -1,22 +1,19 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        List<Integer> arr = new ArrayList<>();
         int i = 1;
+        int ans = 0;
+
         while(i < nums.length-1){
             int j = i;
             while(j < nums.length-1 && nums[j] - nums[j-1] == nums[j+1] - nums[j]){
                 j++;
             }
-            if (j > i) {
-                arr.add(j + 1 - (i - 1));
+            if(j > i){
+                int num = j+1 - (i-1);
+                ans += (num-1)*(num-2)/2;
             }
-            
-            i = j+1;
-        }
-        int ans = 0;
 
-        for(int num: arr){
-            ans += (num-1)*(num-2)/2;
+            i = j+1;
         }
 
         return ans;
