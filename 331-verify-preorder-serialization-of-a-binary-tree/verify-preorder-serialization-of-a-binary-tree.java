8f8c53e496d65cab468bf81fcbb9c58f9c720b05
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-07-29 15:53:30 +0530

    Time: 4 ms (95.19%) | Memory: 44.9 MB (65.35%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
@@ -1,32 +1,13 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        if(preorder.length() == 1 && preorder.charAt(0) == '#') return true;
         String nums[] = preorder.split(",");
-        if(preorder.equals("9,3,4,#,#,1,#,#,#,2,#,6,#,#")) return false;
-        
-        Stack<int[]> st = new Stack<>();
-
+        int c = 1;
         for(String s: nums){
-            if(!s.equals("#")){
-                int num = Integer.valueOf(s);
-                if(st.size() == 0) st.add(new int[]{num, 0});
-                else{
-                    int temp[] = st.pop();
-                    temp[1]++;
-                    if(temp[1] != 2) st.add(temp);
-                    st.add(new int[]{num, 0});
-                }
-            }
-            else{
-                if(st.size() == 0) return false;
-                else{
-                    int temp[] = st.pop();
-                    temp[1]++;
-                    if(temp[1] != 2) st.add(temp);
-                }
-            }
+            c--;
+            if(c<0) return false;
+            if(!s.equals("#")) c+=2;
         }
 
-        return st.size() == 0;
+        return c == 0;
     }
 }
