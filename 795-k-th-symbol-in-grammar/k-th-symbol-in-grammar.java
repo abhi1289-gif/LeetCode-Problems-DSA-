a5ae18a8bb0e3fc47ac647a9a8c2f8644684e4b1
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-07-31 21:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (58.91%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        int ans = 0;
+        int flip = 0;
+        while (n > 1) {
+            int half = 1 << (n - 2);
+
+            if (k > half) {
+                flip++;
+                k -= half;
+            }
+
+            n--;
+        }
+
+        return (flip % 2 == 0) ? 0 : 1;
+    }
+}
