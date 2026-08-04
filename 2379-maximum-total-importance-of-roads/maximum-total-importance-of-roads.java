4e5f396e3b5a16708e98bfed891fcce182b3e73a
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-08-04 13:03:07 +0530

    Time: 12 ms (97.47%) | Memory: 117.2 MB (22.28%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -1,25 +1,16 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for(int i=0; i<n; i++) graph.add(new ArrayList<>());
-
+        int nums[] = new int[n];
         for(int road[]: roads){
-            graph.get(road[0]).add(road[1]);
-            graph.get(road[1]).add(road[0]);
+            nums[road[0]]++;
+            nums[road[1]]++;
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)->b-a);
-
-        for(int i=0; i<n; i++) pq.add(graph.get(i).size());
-
-        int a = pq.size();
-
+        Arrays.sort(nums);
         long ans = 0;
 
-        while(pq.size() > 0){
-            ans += (long)pq.poll()*a;
-            a--;
+        for(int i=0; i<nums.length; i++){
+            ans += (long)nums[i]*(i+1);
         }
 
         return ans;
