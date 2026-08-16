4541ac07722fe8c776553da847b95818266c6981
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-08-16 17:00:00 +0530

    Time: 4 ms (75.68%) | Memory: 114.7 MB (82.43%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -2,14 +2,25 @@ class Solution {
     public boolean stoneGameIX(int[] stones) {
         int nums[] = new int[3];
 
-        for(int x : stones){
-            nums[x % 3]++;
+        for(int x : stones) nums[x % 3]++;
+
+        if (nums[1] == 0 || nums[2] == 0) {
+
+            int nonZero = Math.max(nums[1], nums[2]);
+
+            if (nonZero > 2 && nums[0] % 2 == 1) {
+                return true;
+            }
+
+            return false;
         }
 
-        if(nums[1] == 0 || nums[2] == 0){
-            return Math.max(nums[1], nums[2]) > 2 && nums[0] % 2 == 1;
+        if (nums[0] % 2 == 0) {
+            return true;
         }
 
-        return nums[0] % 2 == 0 || Math.abs(nums[1] - nums[2]) > 2;
+        int difference = Math.abs(nums[1] - nums[2]);
+
+        return difference > 2;
     }
 }
