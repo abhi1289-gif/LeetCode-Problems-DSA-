10c9eae82440abd1769d4e938ec05ccaa8738c29
Abhishek Sonparote <abhisheksonparote6@gmail.com>
2026-09-06 15:42:01 +0530

    Time: 28 ms (5.20%) | Memory: 54.6 MB (27.55%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,21 +4,18 @@ class Solution {
 
     public int travel(int i, int j, String s, String t){
         if(j == t.length()) return 1;
-
         if(i == s.length()) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
         if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = travel(i+1, j, s, t) + travel(i+1, j+1, s, t);
+            return dp[i][j] = travel(i+1, j+1, s, t) + travel(i+1, j, s, t);
         }
 
         return dp[i][j] = travel(i+1, j, s, t);
-
     }
-    
+
     public int numDistinct(String s, String t) {
-        
         dp = new int[s.length()][t.length()];
         for(int i=0; i<s.length(); i++){
             for(int j=0; j<t.length(); j++){
